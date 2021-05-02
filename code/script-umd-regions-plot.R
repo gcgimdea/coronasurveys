eaae01d09b72465674eb574893af6d68c379e188
antonio_fernandez <antonio.fernandez@imdea.org>
2021-05-03 01:26:34 +0200

    plots

--- code/script-umd-regions-plot.R
+++ code/script-umd-regions-plot.R
@@ -8,6 +8,11 @@ estimates_umd_path <- "../data/estimates-symptom-survey/region/"
 plots_path <- "../data/estimates-symptom-survey/plots/"
 
 smooth_param <- 40
+
+cli_factor <- 1
+cli_local_factor <- 1
+anosmia_factor <- 1
+
 # start_date <- "2020-04-10"
 start_date <- "2021-01-01"
 #end_date <- "2020-12-10"
@@ -47,22 +52,22 @@ plot_region <- function(country = "IN", region = "Rajasthan")
   # colors <- c("Nuevos casos" = "red", "recent_c" = "red", "sick_c" = "blue", "Sintomáticos" = "blue")
   p1 <- ggplot(data = df_umd, aes(x = date, color = ""))  +
     # geom_point(aes(y = p_cli*100000, color = "UMD CLI"), alpha = 0.5, size = 2) +
-    geom_line(aes(y = p_cli_14days*100000, color = "UMD CLI"),
+    geom_line(aes(y = cli_factor * p_cli_14days*100000, color = "UMD CLI"),
               linetype = "solid", size = 1, alpha = 0.6) +
-    geom_ribbon(aes(ymin = p_cli_14days_low*100000,
-                    ymax = p_cli_14days_high*100000),
+    geom_ribbon(aes(ymin = cli_factor * p_cli_14days_low*100000,
+                    ymax = cli_factor * p_cli_14days_high*100000),
                 alpha = 0.1, color = "green", size = 0.1, fill = "green") +
     # geom_point(aes(y = p_cli_local*100000, color = "UMD CLI Indirect"), alpha = 0.5, size = 2) +
-    geom_line(aes(y = p_cli_local_14days*100000, color = "UMD CLI Indirect"),
+    geom_line(aes(y = cli_local_factor * p_cli_local_14days*100000, color = "UMD CLI Indirect"),
               linetype = "solid", size = 1, alpha = 0.6) +
-    geom_ribbon(aes(ymin = p_cli_local_14days_low*100000,
-                    ymax = p_cli_local_14days_high*100000),
+    geom_ribbon(aes(ymin = cli_local_factor * p_cli_local_14days_low*100000,
+                    ymax = cli_local_factor * p_cli_local_14days_high*100000),
                 alpha = 0.1, color = "red", size = 0.1, fill = "red") +
     # geom_point(aes(y = p_anosmia*100000, color = "UMD anosmia"), alpha = 0.5, size = 2) +
-    geom_line(aes(y = p_anosmia_14days*100000, color = "UMD anosmia"),
+    geom_line(aes(y = anosmia_factor * p_anosmia_14days*100000, color = "UMD anosmia"),
               linetype = "solid", size = 1, alpha = 0.6) +
-    geom_ribbon(aes(ymin = p_anosmia_14days_low*100000,
-                    ymax = p_anosmia_14days_high*100000),
+    geom_ribbon(aes(ymin = anosmia_factor * p_anosmia_14days_low*100000,
+                    ymax = anosmia_factor * p_anosmia_14days_high*100000),
                 alpha = 0.1, color = "blue", size = 0.1, fill = "blue") +
     labs(x = "Date", y =  "Cases per 100,000 people") +
     # ylim(0, 3000)+
@@ -130,7 +135,7 @@ plot_region <- function(country = "IN", region = "Rajasthan")
 
 #----------
 
-regions <- c(
+regions_india <- c(
 "Andaman and Nicobar",
 "Andhra Pradesh",
 "Arunachal Pradesh",
@@ -170,7 +175,40 @@ regions <- c(
 "West Bengal"
 )
 
-for (r in regions) {
+for (r in regions_india) {
   plot_region(country = "IN", region = r)
 }
 
+regions_brazil <- c(
+  "Acre",
+  "Alagoas",
+  "Amapá",
+  "Amazonas",
+  "Bahia",
+  "Ceará",
+  "Distrito Federal",
+  "Espírito Santo",
+  "Goiás",
+  "Maranhão",
+  "Mato Grosso",
+  "Mato Grosso do Sul",
+  "Minas Gerais",
+  "Pará",
+  "Paraíba",
+  "Paraná",
+  "Pernambuco",
+  "Piauí",
+  "Rio de Janeiro",
+  "Rio Grande do Norte",
+  "Rio Grande do Sul",
+  "Rondônia",
+  "Roraima",
+  "Santa Catarina",
+  "São Paulo",
+  "Sergipe",
+  "Tocantins"
+)
+
+for (r in regions_brazil) {
+  plot_region(country = "BR", region = r)
+}
