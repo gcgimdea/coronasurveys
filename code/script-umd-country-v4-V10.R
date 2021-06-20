89b478d697194cc6530c7d9e618bbeda856e43b9
antonio_fernandez <antonio.fernandez@imdea.org>
2021-06-20 19:20:05 +0200

    umd

--- code/script-umd-country-v4-V10.R
+++ code/script-umd-country-v4-V10.R
@@ -7,18 +7,24 @@ library(data.table)
 library(tidyverse)
 
 responses_path2020 <- "/gauss_data/coronasurveys/UMD-2020/aggregates/country/"
-responses_path2021 <- "/gauss_data/coronasurveys/UMD-2021-v11/aggregates/country/"
+responses_path2021 <- "/gauss_data/coronasurveys/UMD-2021/aggregates-v10/country/"
 # data_path <- "https://raw.githubusercontent.com/GCGImdea/coronasurveys/master/data/common-data/unified-country-list.csv"
-estimates_path <- "../data/estimates-symptom-survey/PlotData-v11/"
+estimates_path <- "../data/estimates-symptom-survey/PlotData-v10/"
 
 files2020 <- list.files(path=responses_path2020, pattern="*.csv", full.names=FALSE, recursive=FALSE)
 files2021 <- list.files(path=responses_path2021, pattern="*.csv", full.names=FALSE, recursive=FALSE)
 files <- unique(c(files2020, files2021))
 
-cols_to_use <- c("ISO2",	"ISO_3",	"country_agg",	"date",	"first_date",	"count", "day_count",	
+cols_to_use <- c("ISO2",	"ISO_3",	"country_agg",	"date",	"first_date",	"count", "day_count",	"days_aggregated",
                  "p_cli",	"p_cli_CI",	"p_cli_weight",	"p_cli_weight_CI",	
                  "p_cliWHO",	"p_cliWHO_CI",	"p_cliWHO_weight",	"p_cliWHO_weight_CI",	
                  "p_cli_local",	"p_cli_local_CI")
+
+character_cols <- c("ISO2",	"ISO_3",	"country_agg")
+date_cols <- c("date", "first_date")
+numeric_cols <- c("count", "day_count", "days_aggregated", "p_cli",	"p_cli_CI",	"p_cli_weight",	"p_cli_weight_CI",	
+                  "p_cliWHO",	"p_cliWHO_CI",	"p_cliWHO_weight",	"p_cliWHO_weight_CI",	"p_cli_local",	"p_cli_local_CI")
+
 
 smooth_param <- 15
 
@@ -41,19 +47,23 @@ process_country <- function(file) {
   
   if (file.exists(f2020)) {
     DT2020 <- fread(f2020)
+    DT2020 <- DT2020[,date:=as.Date(date)]
+    DT2020 <- DT2020[,first_date:=as.Date(first_date)]
     DT2020 <- DT2020[date >= "2020-01-01" & date <= "2020-12-31", ..cols_to_use]
   } else {
     DT2020 <- data.table() 
   }
   if (file.exists(f2021)) {
     DT2021 <- fread(f2021)
+    DT2021 <- DT2021[,date:=as.Date(date)]
+    DT2021 <- DT2021[,first_date:=as.Date(first_date)]
     DT2021 <- DT2021[date >= "2021-01-01" & date <= "2021-12-31", ..cols_to_use]
   } else {
     DT2021 <- data.table() 
   }
   DT <- rbindlist(list(DT2020, DT2021), use.names=TRUE)
   
-  DT <- DT[,date:=as.Date(date)]
+  DT <- DT[order(date)]
   
   DT <- DT[, p_cli_smooth := 
              with(DT, ksmooth(date, p_cli, kernel = "normal", bandwidth = smooth_param, x.points=date))$y]
--- code/script-umd-region-v2-V11.R
+++ code/script-umd-region-v2-V11.R
@@ -7,18 +7,23 @@ library(data.table)
 library(tidyverse)
 
 responses_path2020 <- "/gauss_data/coronasurveys/UMD-2020/aggregates/region/"
-responses_path2021 <- "/gauss_data/coronasurveys/UMD-2021-v10/aggregates/region/"
+responses_path2021 <- "/gauss_data/coronasurveys/UMD-2021/aggregates-v11/region/"
 # data_path <- "https://raw.githubusercontent.com/GCGImdea/coronasurveys/master/data/common-data/unified-country-list.csv"
-estimates_path <- "../data/estimates-symptom-survey/region-v10/"
+estimates_path <- "../data/estimates-symptom-survey/region-v11/"
 
 files2020 <- list.files(path=responses_path2020, pattern="*.csv", full.names=FALSE, recursive=FALSE)
 files2021 <- list.files(path=responses_path2021, pattern="*.csv", full.names=FALSE, recursive=FALSE)
 files <- unique(c(files2020, files2021))
 
-cols_to_use <- c("ISO2",	"ISO_3",	"country_agg",	"region_agg", "date",	"first_date",	"count", "day_count",	
+cols_to_use <- c("ISO2",	"ISO_3",	"country_agg",	"region_agg", "date", "first_date", "count", "day_count", "days_aggregated",
                  "p_cli",	"p_cli_CI",	"p_cli_weight",	"p_cli_weight_CI",	
                  "p_cliWHO",	"p_cliWHO_CI",	"p_cliWHO_weight",	"p_cliWHO_weight_CI",	
                  "p_cli_local",	"p_cli_local_CI")
+
+character_cols <- c("ISO2",	"ISO_3",	"country_agg",	"region_agg")
+date_cols <- c("date", "first_date")
+numeric_cols <- c("count", "day_count", "days_aggregated", "p_cli",	"p_cli_CI",	"p_cli_weight",	"p_cli_weight_CI",	
+                  "p_cliWHO",	"p_cliWHO_CI",	"p_cliWHO_weight",	"p_cliWHO_weight_CI",	"p_cli_local",	"p_cli_local_CI")
 
 smooth_param <- 15
 
@@ -42,7 +47,16 @@ process_country <- function(file) {
     df <- fread(f2020, data.table = F)
     df <- df %>% 
       dplyr::select(all_of(cols_to_use))
-    df$date <- as.Date(df$date)
+    for (c in character_cols) {
+      df[[c]] <- as.character(df[[c]])
+    }
+    for (c in date_cols) {
+      df[[c]] <- as.Date(df[[c]])
+    }
+    for (c in numeric_cols) {
+      df[[c]] <- as.numeric(df[[c]])
+    }
+    # df$date <- as.Date(df$date)
     df <- df[which((df$date >= as.Date("2020-01-01")) & (df$date <= as.Date("2020-12-31"))),]
     # cat("2020:", dim(df), "\n")
   }
@@ -50,7 +64,16 @@ process_country <- function(file) {
     df2 <- fread(f2021, data.table = F)
     df2 <- df2 %>% 
       dplyr::select(all_of(cols_to_use))
-    df2$date <- as.Date(df2$date)
+    for (c in character_cols) {
+      df2[[c]] <- as.character(df2[[c]])
+    }
+    for (c in date_cols) {
+      df2[[c]] <- as.Date(df2[[c]])
+    }
+    for (c in numeric_cols) {
+      df2[[c]] <- as.numeric(df2[[c]])
+    }
+    # df2$date <- as.Date(df2$date)
     df2 <- df2[which((df2$date >= as.Date("2021-01-01")) & (df2$date <= as.Date("2021-12-31"))),]
     df <- dplyr::bind_rows(df, df2)
     # cat("2021:", dim(df), "\n")
@@ -63,7 +86,7 @@ process_country <- function(file) {
   for (r in regions) {
     dfr <- df[which(df$region_agg == r),]
     cat(r, " ")
-    
+    dfr <- dfr[order(dfr$date),]
     # cat("Smoothing...")
     dfr$p_cli_smooth <- 
       with(dfr, ksmooth(date, p_cli, kernel = "normal", bandwidth = smooth_param, x.points=date))$y
@@ -99,7 +122,9 @@ process_country <- function(file) {
     }
     # cat(dim(dfTotal), "\n")
   }
-  fwrite(dfTotal, paste0(estimates_path, file))
+  if (is.data.frame(dfTotal)) {
+    fwrite(dfTotal, paste0(estimates_path, file))
+  }
 }
 
 kk <- lapply(files, process_country)
