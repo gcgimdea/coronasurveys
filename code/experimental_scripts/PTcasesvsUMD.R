8bb542fbc1d7841fd32cce7dd90e31fbe2074b07
CBaquero <cbm@di.uminho.pt>
2021-07-21 17:06:42 +0100

    improved script

--- code/experimental_scripts/PTcasesvsUMD.R
+++ code/experimental_scripts/PTcasesvsUMD.R
@@ -1,13 +1,18 @@
 library(dplyr)
 library(BBmisc) # maybe for more compact normalization later
 library(zoo) # to use rollmean
+#library(naniar) # replace_with_na_at
 
-dataumd <- read.csv("../../data/estimates-symptom-survey/PlotData/PT-estimate.csv")
+dataumd <- read.csv("../../data/estimates-symptom-survey/PlotData/IR-estimate.csv")
 dataumd$date <- as.Date(dataumd$date)
+
+# Prune days
+#for (i in seq(422,429))
+#  dataumd$p_cli[i] <- NA
 
 #plot(data$pct_cli_smooth)
 
-dataccfr <- read.csv("../../data/estimates-ccfr-based/PlotData/PT-estimate.csv")
+dataccfr <- read.csv("../../data/estimates-ccfr-based/PlotData/IR-estimate.csv")
 dataccfr$date <- as.Date(dataccfr$date)
 pop <- dataccfr$population[1]
 
@@ -16,26 +21,47 @@ joined <- dataccfr %>% dplyr::select(date, cases, cases_daily) %>% inner_join( d
 #joined <- dataccfr %>% dplyr::select(date, cases, cases_daily) %>% inner_join( dataumd %>% dplyr::select(date, p_cli, p_cli_smooth,p_cli_smooth_slope) , by="date")
 
 
-# Cooy series to normalize
-c <- rollmean(joined$cases,7)
-r <- joined$p_cli_smooth
-rr <- rollmean(joined$p_cli,7)
-rs <- joined$p_cli_smooth_slope
-rs[is.na(rs)] <- 0
+
+# Copy series to normalize
+c <- rollmean(joined$cases,1)
+#r <- joined$p_cli_smooth
+#rr <- rollmean(joined$p_cli,7)
+#rs <- joined$p_cli_smooth_slope
+cli_ks <- ksmooth(joined$date,joined$p_cli, "normal", bandwidth = 30)
+cases_ks <- ksmooth(joined$date,joined$cases, "normal", bandwidth = 20)
+#rs[is.na(rs)] <- 0
+
+
 
 #Normalize each to 0-1 scale
-c <- (c-min(c))/(max(c)-min(c))
-r <- (r-min(r))/(max(r)-min(r))
-rr <- (rr-min(rr))/(max(rr)-min(rr))
-rs <- (rs-min(rs))/(max(rs)-min(rs))
+#c <- (c-min(c))/(max(c)-min(c))
+#r <- (r-min(r))/(max(r)-min(r))
+#rr <- (rr-min(rr))/(max(rr)-min(rr))
+#rs <- (rs-min(rs))/(max(rs)-min(rs))
+#rks <- (rks$y-min(rks$y))/(max(rks$y)-min(rks$y))
+c <- normalize(c,method="range")
+cli_ks <- normalize(cli_ks$y, method="range")
+cases_ks <- normalize(cases_ks$y, method="range")
 
-plot(c,col="black")
-lines(r,col="blue")
-lines(rs,col="red")
-lines(rr,col="green")
+# Make 1st derivative
+cli_ksd <- diff(cli_ks,lag=4)
+cases_ksd <- diff(cases_ks,lag=4)
+cli_ksd <- normalize(cli_ksd, method="range")
+cases_ksd <- normalize(cases_ksd, method="range")
+
+plot(c,col="black",type="p")
+lines(cases_ksd,col="gray")
+lines(cli_ksd,col="light blue")
+#lines(rr,col="green")
+lines(cli_ks,col="blue")
+#lines(rksd,col="orange")
+lines(cases_ks,col="black")
 
 print(min(joined$date))
 print(max(joined$date))
 print(max(dataumd$date))
+
+#plot(joined$date,joined$p_cli)
+#lines(ksmooth(joined$date,joined$p_cli, "normal", bandwidth = 14), col = 2)
 
 
