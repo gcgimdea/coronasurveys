12f1af3db76e6b74393ddd7c7bb7a833912792c1
antonio_fernandez <antonio.fernandez@imdea.org>
2021-11-22 09:45:47 +0100

    participation

--- code/participation-ranking-v2.R
+++ code/participation-ranking-v2.R
@@ -13,23 +13,38 @@ compute_ranking <- function(country_geoid){
   cat(country_geoid, "\n")
   file_name <- paste0(aggregates_path, country_geoid, "-aggregate.csv")
   if (file.exists(file_name)) {
-    dt <- read.csv(file_name, as.is = T, encoding = "UTF-8") %>% select("Timestamp","ISO.3166.2","Cookie") %>% 
-      filter(Timestamp > "2019/12/31") %>%  filter(Cookie != "")
+    dt <- read.csv(file_name, as.is = T, encoding = "UTF-8") %>% 
+     select("Timestamp","Country", "Cookie") %>%
+      filter(Timestamp > "2019/12/31") %>%  
+      filter(Cookie != "")
     if (nrow(dt)>0) {
       dt$Timestamp<-as.Date(dt$Timestamp)
       dt <- unique.data.frame(dt)
-      dt <- dt %>% group_by(Cookie) %>% summarise(Count=n()) %>% arrange(desc(Count))
-      dt = subset(dt, select = c(Cookie,Count) )
+      dt <- dt %>% group_by(Country,Cookie) %>% summarise(Count=n()) %>% arrange(desc(Count))
+      dt = subset(dt, select = c(Country,Cookie,Count) )
       write.csv(dt,paste0(ranking_path, country_geoid, "-ranking.csv"),row.names = FALSE)
+    } else {
+      dt <- data.frame()
     }
+  } else {
+    dt <- data.frame()
   }
+  return(dt)
 }
-
+  
 # data_country_code <- read_excel(country_codes_file)
 # names(data_country_code) <- c("English.short.name.lower.case", "Alpha.2.code",
 #                               "Alpha.3.code", "Numeric.code", "ISO.3166.2")
 # all_geo_ids <- unique(data_country_code$Alpha.2.code)
 allFiles <- list.files(aggregates_path, pattern = "*-aggregate.csv")
 all_geo_ids <- unique(substr(allFiles,1,2))
-go <- sapply(all_geo_ids, compute_ranking)
+#go <- sapply(all_geo_ids, compute_ranking)
+overall <- data.frame()
+for (iso2 in all_geo_ids) {
+  df <- compute_ranking(iso2)
+  overall <- rbind(overall, df)
+}
+overall <- overall[order(overall$Count, decreasing = TRUE),]
+write.csv(overall,paste0(ranking_path, "overall-ranking.csv"),row.names = FALSE)
+  
 
