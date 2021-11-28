038f5a2085c2afc8cd8bdcd279377d1b9067c055
antonio_fernandez <antonio.fernandez@imdea.org>
2021-11-28 11:06:45 +0100

    confirmed

--- code/script-confirmed3.R
+++ code/script-confirmed3.R
@@ -28,7 +28,7 @@ estimates_path <- "../data/estimates-confirmed/PlotData/"
 # data_path_Ox <- "../coronasurveys/data/oxford/PlotData/"
 # estimates_path <- "./estimates-confirmed/"
 
-smooth_param <- 30
+smooth_param <- 15
 
 contagious_window <- 10 # Changed from 12 on Nov 28th, 2021
 active_window <- 10 # Changed from 18 on May 30th, 2021
