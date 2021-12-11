7ba6369ad574f8b731d170cee3873c78fc69331b
antonio_fernandez <antonio.fernandez@imdea.org>
2021-12-11 10:42:34 +0100

    Remove oxford regions and intervention columns

--- code/script-get-oxford-data.R
+++ code/script-get-oxford-data.R
@@ -8,7 +8,7 @@ library(readr)
 
 DATA_URL = "https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest.csv"
 country_file <- "../data/common-data/country_oxford.csv"
-region_file <- "../data/common-data/region_oxford.csv"
+# region_file <- "../data/common-data/region_oxford.csv"
 data_file <- "../data/common-data/oxford-umd-country-population.csv"
 output_path = "../data/oxford/"
 
@@ -28,7 +28,7 @@ data_ox <- data_ox %>% mutate(Date = paste0( str_sub(Date, 1, 4), "-",
 
 
 df_country <- data_ox[data_ox$Jurisdiction == "NAT_TOTAL",]
-df_region <- data_ox[data_ox$Jurisdiction == "STATE_TOTAL",]
+# df_region <- data_ox[data_ox$Jurisdiction == "STATE_TOTAL",]
 
 # Generates files with the list of countries and regions
 # df_country <- df_country %>%
@@ -40,14 +40,14 @@ write.csv(df_country %>%
           file = "../data/common-data/country_oxford.csv",
           row.names = FALSE)
 
-# df_region <- df_region %>%
-#   select(RegionName, RegionCode)  %>%
-#   distinct()
-write.csv(df_region %>%
-            select(RegionName, RegionCode)  %>%
-            distinct(),
-          file = "../data/common-data/region_oxford.csv",
-          row.names = FALSE)
+# # df_region <- df_region %>%
+# #   select(RegionName, RegionCode)  %>%
+# #   distinct()
+# write.csv(df_region %>%
+#             select(RegionName, RegionCode)  %>%
+#             distinct(),
+#           file = "../data/common-data/region_oxford.csv",
+#           row.names = FALSE)
 
 c_data <- read.csv(data_file, as.is = T)
 
@@ -65,22 +65,26 @@ for (country in all_countries) {
   geoid <- c_data[c_data$CountryName == country,"geo_id"]
   df$population <- c_data[c_data$CountryName == country,"population"]
   df$iso2 <- geoid
+  df <- df %>% select(CountryName, CountryCode, Date, ConfirmedCases, ConfirmedDeaths, 
+                cases, deaths, population, iso2)
   write.csv(df, paste0(output_path, "PlotData/", geoid, "-estimate.csv"),
             row.names = FALSE)
 }
 
-region_list <- read.csv(region_file, as.is = T)
-all_regions <- region_list$RegionName
-for (region in all_regions) {
-  cat("Processing", region, "\n")
-  df <- df_region[df_region$RegionName == region,]
-  df$cases <- c(NA,diff(df$ConfirmedCases))
-  df$deaths <- c(NA,diff(df$ConfirmedDeaths))
-  region_code <- df$RegionCode[1]
-  df$population <- region_list[region_list$RegionName == region,"Population"]
-  df$iso2 <- region_code
-  write.csv(df, paste0(output_path, "region/", region_code, "-estimate.csv"),
-            row.names = FALSE)
-}
+# region_list <- read.csv(region_file, as.is = T)
+# all_regions <- region_list$RegionName
+# for (region in all_regions) {
+#   cat("Processing", region, "\n")
+#   df <- df_region[df_region$RegionName == region,]
+#   df$cases <- c(NA,diff(df$ConfirmedCases))
+#   df$deaths <- c(NA,diff(df$ConfirmedDeaths))
+#   region_code <- df$RegionCode[1]
+#   df$population <- region_list[region_list$RegionName == region,"Population"]
+#   df$iso2 <- region_code
+#   df <- df %>% select(CountryName, CountryCode, RegionName, RegionCode, Date, 
+#                 ConfirmedCases, ConfirmedDeaths, cases, deaths, iso2)
+#   write.csv(df, paste0(output_path, "region/", region_code, "-estimate.csv"),
+#             row.names = FALSE)
+# }
 
 
